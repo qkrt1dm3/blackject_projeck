083ff16583633517126a6700c5ff17462fc4d1a3
sieun <parksieun0203@gmail.com>
2026-06-06 14:50:33 +0900

    first commit

---
+++ project.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+typedef struct {
+    char name[30];
+    char password[20];
+    int asset;
+} Player;
+
+typedef struct {
+    char suit[10];
+    char rank[5];
+    int value;
+} Card;
+
+
+
+
+int main(void) {
+
+}
